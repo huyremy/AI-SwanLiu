0b6ae0391b3797b816a894993f6ce347b431bc65
Nguyễn Quang Huy <huynq@isi.com.vn>
2023-02-04 06:45:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
 
-model = load_model('model.h5')
+model = load_model('model.h5', compile=False)
 class_names=["YES","NO"]
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-image = Image.open('test.jpg').convert('RGB')
+image = Image.open('20230204_061206.jpg').convert('RGB')
 image = ImageOps.fit(image,(224, 224), Image.ANTIALIAS)
 image_array = np.asarray(image)
 
@@ -17,5 +19,7 @@ prediction = model.predict(data)
 
 index = np.argmax(prediction)
 class_name = class_names[index]
-
+score = prediction[0][index]
+score = round(score * 100, 3)
 print("Tình trạng bệnh : ", class_name)
+print("Khả năng chữa khỏi: ", score, "%")
